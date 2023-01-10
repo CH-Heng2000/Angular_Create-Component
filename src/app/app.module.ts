97a630e92a60522f5736b51bff81ff270b34cc4b
Heng Chun Horn <chun-horn.heng@vitrox.local>
2023-01-10 10:28:12 +0800

    Learned create components and applied style to them.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
